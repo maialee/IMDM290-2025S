42b1b3cd2587dd469d730a53a2d2dc1d4487ee24
maialee <maialee8@terpmail.umd.edu>
2025-03-13 10:24:51 -0400

    new changes

--- Assets/Scenes/HeartScript.cs
+++ Assets/Scenes/HeartScript.cs
@@ -1,3 +1,4 @@
+
 // UMD IMDM290 
 // Instructor: Myungin Lee
     // [a <-----------> b]
@@ -8,10 +9,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lerp2 : MonoBehaviour
+public class HeartScript : MonoBehaviour
 {
+    public float r;
     LineRenderer[] lineRenderer;
-    static int numSphere = 500; 
+    static int numSphere = 200; 
     float time = 0f;
     Vector3[] initPos;
     Vector3[] startPosition, endPosition;
@@ -30,21 +32,19 @@ public class Lerp2 : MonoBehaviour
         // Define target positions. Start = random, End = heart 
         for (int i =0; i < numSphere; i++){
             // Random start positions
-            float r = 25f;
-            startPosition[i] = new Vector3(r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f));        
+            startPosition[i] = new Vector3( Random.Range(-0.5f, 0.5f), Random.Range(0.5f, 0.5f), Random.Range(-0.5f, 0.5f));        
             // Heart shape end position
             t = i* 2 * Mathf.PI / numSphere;
             endPosition[i] = new Vector3( 
-                        5f*Mathf.Sqrt(2f) * Mathf.Sin(t) *  Mathf.Sin(t) *  Mathf.Sin(t),
-                        5f* (- Mathf.Cos(t) * Mathf.Cos(t) * Mathf.Cos(t) - Mathf.Cos(t) * Mathf.Cos(t) + 2 *Mathf.Cos(t)) + 3f,
+                        r *Mathf.Sqrt(2f) * Mathf.Sin(t) *  Mathf.Sin(t) *  Mathf.Sin(t),
+                        r * (- Mathf.Cos(t) * Mathf.Cos(t) * Mathf.Cos(t) - Mathf.Cos(t) * Mathf.Cos(t) + 2 *Mathf.Cos(t)) + 3f,
                         10f + Mathf.Sin(time));
         }
+        
         // Let there be spheres..
         for (int i =0; i < numSphere; i++){
-            float r = 10f; // radius of the circle
             // Position
             initPos[i] = startPosition[i];
-            // Color
         
         }
         // Line
@@ -52,16 +52,11 @@ public class Lerp2 : MonoBehaviour
             //For creating line renderer object
             lineRenderer[i] = new GameObject("Line").AddComponent<LineRenderer>();
             lineRenderer[i].material = new Material(Shader.Find("Sprites/Default"));
-            // lineRenderer[i].startColor = Color.HSVToRGB(Random.Range(0.7f, 1f), Random.Range(0.8f, 1f), Random.Range(0f, 1f));
             lineRenderer[i].endColor = Color.HSVToRGB(Random.Range(0.8f, 1f), Random.Range(0.8f, 0.94f), Random.Range(3f, 5f));
-            // lineRenderer[i].startWidth = 0.3f;
-            // lineRenderer[i].endWidth = 0.4f;
-            // lineRenderer[i].positionCount = 2;
-            // lineRenderer[i].useWorldSpace = true;    
+            lineRenderer[i].startWidth = 0.3f;
+            lineRenderer[i].endWidth = 0.4f;
+            lineRenderer[i].positionCount = 2;
                             
-            //For drawing line in the world space, provide the x,y,z values
-            lineRenderer[i].SetPosition(0, initPos[i]); //x,y and z position of the starting point of the line
-            lineRenderer[i].SetPosition(1, initPos[i-1]); //x,y and z position of the end point of the line
         }
 
     }
@@ -70,7 +65,7 @@ public class Lerp2 : MonoBehaviour
     void Update()
     {
         // Measure Time 
-        time += Time.deltaTime; // Time.deltaTime = The interval in seconds from the last frame to the current one
+        time += Time.deltaTime*4; // Time.deltaTime = The interval in seconds from the last frame to the current one
         // what to update over time?
         for (int i =1; i < numSphere; i++){
             // Lerp : Linearly interpolates between two points.
@@ -87,10 +82,7 @@ public class Lerp2 : MonoBehaviour
             int randindex = (int)Random.Range(1,numSphere);
             // lineRenderer[i].SetPosition(1, Vector3.Lerp(startPosition[randindex], endPosition[randindex], lerpFraction)); //x,y and z position of the end point of the line
 
-            // For now, start positions and end positions are fixed. But what if you change it over time?
-            // startPosition[i]; endPosition[i];
-
-            // Color Update over time
         }
     }
 }
+
